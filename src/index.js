097c15fd50cde9540deda9078562c37b87b0e2c4
Elena Pedko <elpg.ods@gmail.com>
2023-02-16 16:34:06 +0200

    files commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { team } from './js/team';
+import { searchMovie } from './js/search';
+import { spinner } from './js/spinner';
+import { modalClose } from './js/modalClose';
+import {
+  fetchGenres,
+  getGenresFromLocalStorage,
+  API_KEY,
+} from './js/fetch-and-render-trending';
+
+ import { up } from './js/up';
---
+++ src/js/changeTheme.js
@@ -0,0 +1,31 @@
+const bodyRef = document.querySelector('body');
+const switchToggleRef = document.querySelector('#codepen');
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+loadingThem();
+switchToggleRef.addEventListener('change', chengeTheme);
+
+function chengeTheme() {
+  bodyRef.classList.toggle(Theme.DARK);
+  bodyRef.classList.toggle(Theme.LIGHT);
+
+  getCurrentTheme(bodyRef.classList);
+}
+
+function getCurrentTheme(currentThem) {
+  localStorage.setItem('Theme', currentThem);
+}
+
+function loadingThem() {
+  const savedThem = localStorage.getItem('Theme');
+  if (savedThem === Theme.DARK) {
+    bodyRef.classList.add(savedThem);
+    switchToggleRef.checked = true;
+  } else {
+    bodyRef.classList.add(Theme.LIGHT);
+  }
+}
